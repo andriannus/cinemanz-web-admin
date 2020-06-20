0a261ef7959f36ef0a0eaf6aed88aeea9aa3161b
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-20 13:24:38 +0700

    update login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from '@app/pages/login/login.component';
 
 import { PlainLayoutModule } from '@app/shared/layouts/plain-layout/plain-layout.module';
+import { TextFieldComponent } from '@app/shared/components/text-field/text-field.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, TextFieldComponent],
   imports: [
     PlainLayoutModule,
     RouterModule.forChild([
