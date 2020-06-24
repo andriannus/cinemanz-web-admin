ca0dbedb09121c559e17d0cf3f61c1f7ca978a51
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-24 07:57:58 +0700

    create logout feature

--- src/app/shared/layouts/base-layout/shared/components/base-navbar/base-navbar.component.ts
+++ src/app/shared/layouts/base-layout/shared/components/base-navbar/base-navbar.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { AUTH } from '@app/shared/constants/auth.constant';
+
 @Component({
   selector: 'base-navbar',
   templateUrl: './base-navbar.component.html',
   styleUrls: ['./base-navbar.component.scss'],
 })
-export class BaseNavbarComponent {}
+export class BaseNavbarComponent {
+  logout(): void {
+    localStorage.removeItem(AUTH.token);
+
+    window.location.href = '/login';
+  }
+}
