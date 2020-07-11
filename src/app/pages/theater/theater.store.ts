fcdd344cc6699fea91fd1860cd795257aa1794c7
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-07-11 23:01:53 +0700

    create theater store

---
+++ src/app/pages/theater/theater.store.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { THEATER_STATE } from '@app/pages/theater/theater.constant';
+import { Theater, TheaterState } from '@app/pages/theater/theater.model';
+
+import { Store } from '@app/shared/store';
+import { PaginatedData } from '@app/shared/utils/pagination/pagination.model';
+
+@Injectable()
+export class TheaterStore extends Store<Partial<TheaterState>> {
+  constructor() {
+    super(THEATER_STATE);
+  }
+
+  setTheaters(theaters: PaginatedData<Theater>): void {
+    this.setState({ theaters });
+  }
+}
